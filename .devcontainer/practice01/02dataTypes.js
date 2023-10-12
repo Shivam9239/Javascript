50ffacc9aedc55575b252ae5e5d989bf1436c3d2
Shivam9239 <124271947+Shivam9239@users.noreply.github.com>
2023-10-12 10:33:57 +0000

    datatypes

---
+++ .devcontainer/practice01/02dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // We are using node.js, not browser
+console.log(3 
+    +
+    3)// code readability should  be high, never right code like this
+
+    console.log("Shivam")
+
+
+    let name="Shivam"
+    let age=21
+    let isLoggedIn=false
+    let state;
+
+    //number =>2 to power 53
+    //bigint 
+    //string => ""
+    //boolean =>true
+    //null standalone value // is object type of
+    // undefined =>
+  //symbol 
+
+
+  //object
+
+  console.log(typeof undefined);// undefined
+  console.log( typeof null);// object;
