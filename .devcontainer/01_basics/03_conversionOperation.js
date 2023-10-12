655adc1382f545a553f28cf060075b861f596ebb
Shivam9239 <124271947+Shivam9239@users.noreply.github.com>
2023-10-12 11:07:05 +0000

    Conversion Of Data Types

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="Shivam"
+
+//console.log(typeof score);
+//console.log(typeof (score)); //method
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+//console.log(valueInNumber); //NaN not a number
+
+//"33" => 33
+//"33abc"=> NaN
+// true =>1; false=> 0;
+
+ let isLoggedIn= "Shi"
+ let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+ //1 =>true
+ //0=>false
+ //""=>false
+ //"Shivam"=>true
+
+ let someNumber= "33"
+ let stringNumber=String(someNumber)
+console.log(stringNumber);
