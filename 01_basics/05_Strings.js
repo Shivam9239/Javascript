637778ffdcdffc942303d6980d128dd435d06d1a
Shivam9239 <124271947+Shivam9239@users.noreply.github.com>
2023-10-17 08:28:07 +0000

    string methods

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -5,13 +5,32 @@ const repoCount=3
 //backticks String interpolation
 console.log(`Hello my name  is ${Name} and my repoCount is ${repoCount}`);
 
-const gameName= new String('Shivam')
+const gameName= new String('Shivam-hc-vb')
 // String name  object    constructor
 
-console.log(gameName[0]);
-console.log(gameName.__proto__); //object syntax
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); //object syntax
 
-console.log(gameName.length);//6
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);//6
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('m'));
 
+const newString=gameName.substring(0,4)// not give negative vLUE
+console.log(newString)
 
+const anotherString=gameName.slice(-5,2)//reeverse valuE
+console.log(anotherString);
+
+const newString1=" shivam  "
+console.log(newString1)
+console.log(newString1.trim());
+
+const url="http://hitesh.com/hitesh%20choudhary "
+
+console.log(url.replace('%20','-'));
+
+url.includes('hitesh')
+console.log(url.includes('sundar'));
+
+console.log(gameName.split(""));
