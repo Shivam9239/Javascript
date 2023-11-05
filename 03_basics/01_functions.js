b79c0179e93d04ccc13bca0c31eb8fbf304d296e
Shivam9239 <124271947+Shivam9239@users.noreply.github.com>
2023-11-05 11:46:35 +0000

    functionswithobjects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,3 +47,41 @@ return `${username} just logged in`
 // console.log(loginUserMessage("shivam")
 console.log(loginUserMessage("sk"))
 
+
+//cart
+//                          rest or spread operator
+// function calculateCartPrice(...num1){
+// return num1;
+// }
+
+// console.log(calculateCartPrice(200,400,500));
+
+
+//interveiw perspective
+function calculateCartPrice(val1, val2,...num1){
+    return num1;
+    }
+    
+   // console.log(calculateCartPrice(200,400,500,20000));
+
+
+//    whwenevve handle this type of objecet type safeety is must 
+   const user ={
+    username:"Shivam",
+    prices:"133"
+   }
+   function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+   }
+    
+//    handleObject(user);
+   handleObject({
+    username:"Devson",
+    price:"96"    })
+   
+    //passing array
+const myArray=[210,200,500]
+function returnSecondValue(getarray){
+    return getarray[2]
+}
+console.log(returnSecondValue(myArray));
