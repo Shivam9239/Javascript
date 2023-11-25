56aa6456c92e7d92ee40d095642a12e7836cd735
Shivam9239 <124271947+Shivam9239@users.noreply.github.com>
2023-11-25 09:38:52 +0530

    Update 02dataTypes.js

--- 01_basics/02dataTypes.js
+++ 01_basics/02dataTypes.js
@@ -26,3 +26,6 @@ console.log(3
 
   console.log(typeof undefined);// undefined
   console.log( typeof null);// object;
+
+So null simply meant nothing or void and was represented by all 0's. Hence all its 32 bits were 0's. 
+So whenever the JavaScrit interpreter reads null, it considers the first 3 bits as type “object”. That is why typeof null returns “object”.
